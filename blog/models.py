da4341f1ae224871eb95123b34d997c209e9bec5
Mohsen_Hatami <mohsenhatami13852006@gmail.com>
2026-09-24 17:21:37 +0330

    feat: add article model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.db import models
 from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel, StatusModel
+from model_utils import Choices
 
 class Category(models.Model):
     name = models.CharField("Name", max_length=255)
@@ -7,4 +10,24 @@ class Category(models.Model):
                          unique=True, populate_from="name", always_update=False)
     def __str__(self):
         return self.name
+
+class Article(TimeStampedModel, StatusModel):
+    title = models.CharField(("Title"), max_length=50)
+    content = models.TextField(("Content"))
+    slug = AutoSlugField("Article Address",
+                         unique=True, populate_from="title", always_update=False)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    category = models.ForeignKey(
+        Category,
+        null=True,
+        on_delete=models.CASCADE
+    )
+    STATUS = Choices('draft', 'published')
+    published_at = models.DateTimeField("published", null=True, blank=True)
+    
+    
     
