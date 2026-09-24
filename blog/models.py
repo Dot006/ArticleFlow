a8d26b0b36805cb9bfdd43cb185b2742df8e6cf6
Mohsen_Hatami <mohsenhatami13852006@gmail.com>
2026-09-24 15:04:14 +0330

    feat: add category model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField("Name", max_length=255)
+    slug = AutoSlugField("Category Address",
+                         unique=True, populate_from="name", always_update=False)
+    def __str__(self):
+        return self.name
+    
--- users/models.py
+++ users/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(("Name"), max_length=50)
+    def __str__(self):
+        return self.name
